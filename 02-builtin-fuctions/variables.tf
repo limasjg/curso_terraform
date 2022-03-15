380449319ce333974fd76a65a9a853439600088c
limasjg <joaoguilhermelima7@gmail.com>
2022-03-15 18:34:07 -0300

    build and functions

--- 02-builtin-fuctions/variables.tf
+++ 02-builtin-fuctions/variables.tf
@@ -10,7 +10,40 @@ variable "aws_profile" {
   default = "tf-test"
 }
 
-variable "env" {
-  
+variable "env" {}
+
+variable "instance_ami" {
+  type = string
+  description = ""
+  default = "ami-09e4e8dc0d28dd778"
+
+  validation {
+    condition = length(var.instance_ami) > 4 && substr(var.instance_ami, 0, 4) == "ami-"
+    error_message = "The instance_ami value must be valid AMI id, starting with \"ami-\"."
+  }
+
+}
+
+variable "instance_number" {
+  type = object({
+    dev = number
+    prod = number
+  })
+  description = "Number of instances to create"
+  default = {
+    dev = 1
+    prod = 3
+  }
+}
+variable "instance_type" {
+  type = object({
+    dev = string
+    prod = string
+  })
+  description = ""
+  default = {
+    dev = "t3.micro"
+    prod = "t3.medium"
+  }
 }
 
